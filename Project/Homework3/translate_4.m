e211fe0f06fe8374d68bda3525caf4ecca3e90ac
Nathan <nathanday89@gmail.com>
2018-02-13 18:49:24 -0700

    changed file names

--- Project/Homework3/translate_4.m
+++
@@ -1,3 +0,0 @@
-function pnts = translate(pnts,pn,pe,pd)
-pnts = pnts + repmat([pn,pe,pd],16,1);
-end
--- Project/Homework5/roll_hold.m
+++
@@ -1,3 +0,0 @@
-function delta_a = roll_hold(phi_c, phi, p, P)
-    P.kpphi*(phi_c-phi) + 
-end
